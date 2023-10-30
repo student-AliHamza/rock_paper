fa85390d8001bacd0ec8d1e86f280876f209e89e
student-AliHamza <148673171+student-AliHamza@users.noreply.github.com>
2023-10-30 00:09:09 -0700

    Add files via upload

---
+++ create class using function overriding.cpp
@@ -0,0 +1,21 @@
+#include<iostraem>
+using namespace std;
+class shape{
+	public:
+		int calculate_area(){
+			return 0;
+		}
+	
+	
+};
+int main(){
+	cout<<"enter a number"<<endl;
+	cin>>num1;
+	cout<<"enter a second number"<<endl;
+	cin>>num2;
+	
+	
+	
+	
+	return 0;
+}
